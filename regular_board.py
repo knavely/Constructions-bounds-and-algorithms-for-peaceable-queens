90955bb763aa9ba0826adeec964a2bc5669c758b
knavely <knavely@gmail.com>
2024-09-11 23:37:26 -0700

    cleanup

--- regular_board.py
+++ regular_board.py
@@ -33,26 +33,12 @@ t = 0
 
 m.Equation(y[2**R | 2**C] == y[2**R]*y[2**C]) #y_R\cap C = y_Ry_C
 
-#m.Equation(y[2**D0 | 2**A0] <= y[2**D0]*y[2**A0]) #y_D0\cap A0 = y_D0y_A0
-
-#m.Equation(y[2**D1 | 2**A1] <= y[2**D1]*y[2**A1]) #y_D1\cap A1 = y_D1y_A1
 
 for o in O:
    for x in X:
         m.Equation((y[2**o]**2)/4 + m.if3(1/4 - y[2**x],0,1)*m.if3(2*m.sqrt(y[2**x])-y[2**o],0,1) * y[2**x | 2**o] <= y[2**o] * m.sqrt(y[2**x]))
-        #m.Equation(m.if3(1/4 - y[2**x],0,1)*m.if3(1 - m.sqrt(2 - 4*y[2**x])-y[2**o],0,1)*y[2**o|2**x] <= y[2**o]/2 )
-        #m.Equation((m.sqrt(2-4*y[2**x]) + y[2**o] - 1)**2 + 4*m.if3(1/4 - y[2**x],0,1)*m.if3(m.sqrt(2 - 4*y[2**x]) + y[2**o]-1,0,1)*y[2**o|2**x] <= 2*y[2**o])
-     #   m.Equation(y[2**o | 2**x] <= y[2**o]*y[2**x]) #y_o\cap x <= y_oy_x' for o in O, x in X (X is X from paper, O is square)
-        
-        #m.Equation(m.if2(1/4 - y[2**x],0,1) * ((y[2**o] + 2*m.sqrt(y[2**x]) - 1) * m.abs2(y[2**o]+2*m.sqrt(y[2**x]) - 1))/4 <= y[2**x | 2**o])
 
-        #m.Equation(m.if2(y[2**x] - 1/4,0,1) * m.if2(y[2**o] - m.sqrt(2-4*y[2**x]),0,1)*(y[2**x] - (1-m.sqrt(2-4*y[2**x]))/2) <= y[2**o | 2**x])
-
-        #m.Equation(m.if2(y[2**x] - 1/4,0,1) * m.if2(m.sqrt(2-4*y[2**x])-y[2**o],0,1)*((y[2**o]*(1-m.sqrt(2-4*y[2**x]))/2)+(y[2**o]**2)/4) <= y[2**o | 2**x])
-        
-        #m.Equation((m.sqrt(2*y[2**D0]) + m.sqrt(2*y[2**A0])-1)* m.abs2(m.sqrt(2*y[2**D0]) + m.sqrt(2*y[2**A0])-1)/2 <= y[2**D0|2**A0])
-
-        #m.Equation((m.sqrt(2*y[2**D1]) + m.sqrt(2*y[2**A1])-1)* m.abs2(m.sqrt(2*y[2**D1]) + m.sqrt(2*y[2**A1])-1)/2 <= y[2**D1|2**A1])
+        # m.Equation(y[2**o | 2**x] <= y[2**o]*y[2**x]) #y_o\cap x <= y_oy_x' for o in O, x in X (X is X from paper, O is square) -- gives 7/48 but the constraint is not valid
 
 for a in [D0,A0]:
     for b in [D1,A1] :
@@ -63,11 +49,8 @@ for x in X:
 
 m.Equation(z[2**R | 2**C | 2**D0 | 2**A0 ]+z[2**R | 2**C | 2**D1 | 2**A1] >= z[0])    
 m.Maximize(z[0])
-#m.Maximize(m.min2(z[15], z[0]))
 
 m.options.SOLVER=1
-#m.options.OTOL = 1e-12
-#m.solver_options = ['tol 0.0000000001']
 m.solve()
 
 
@@ -78,13 +61,13 @@ print("")
 
 
 print("*****")
-print("Validating no better solution found....") 
+print("Validating no better  upper bound is feasible....") 
 print("*****")
 print("")
 try:
     m.Equation(z[0] > 0.172)    
     m.solve()
-except: print("better solution does not exist")
+except: print("better upper bound beyond numeric error is infeasible")
 
 def printsol(sol,name):
     for i in range(64):
